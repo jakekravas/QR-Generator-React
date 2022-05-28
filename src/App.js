641aaae08ad8304ad680a92aea8f63b0a1682f4c
Jake Kravas <jakekravas@Jakes-MacBook-Air.local>
2022-05-28 02:59:21 -0700

    implemented QR functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+import QRCode from 'qrcode';
 
 const App = () => {
+  const [url, setUrl] = useState('')
+  const [qrCode, setQrCode] = useState('')
+
+  const generateQrCode = () => {
+    QRCode.toDataURL(url, {
+      width: 800
+    },
+      (err, url) => {
+      if (err) return console.error(err)
+
+      console.log(url);
+      setQrCode(url)
+    })
+  }
+
   return (
     <div className='app'>
       <h1>QR Code Generator</h1>
-      {/* <input type='asdf' /> */}
+      <input
+        type='text'
+        placeholder='e.g. https://google.com'
+        value={url}
+        onChange={e => setUrl(e.target.value)}
+      />
+      <button onClick={generateQrCode}>Generate</button>
+      {qrCode &&
+        <>
+          <img src={qrCode}/>
+          <a href={qrCode} download='qrcode.png'>Download</a>
+        </>
+      }
     </div>
   );
 }
